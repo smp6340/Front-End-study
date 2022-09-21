c556b3bf3935599c9d379f06ca0b5cbc16b1a783
smp6340 <smp6340@naver.com>
2022-09-21 20:18:44 +0900

    📚  : SweetAlert library 공부

---
+++ react-library-test/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import swal from 'sweetalert';
+
+//style
+import * as S from './style';
+
+function App() {
+  const style_alert = () => {
+    // 세 번째 인수 값으로 'success', 'warning', 'error', 'info' 등을 넣을 수 있음. 
+    swal(" 로그인에 성공하였습니다! ", " Anonymous ", "success");
+  }
+
+  const alert_option = () => {
+    swal({
+      title: "Login Success!",
+      text: "Anonymous",
+      icon: "success",
+      button: "next"
+    });
+  }
+
+  const alert_promise = () => {
+    swal("버튼을 클릭하거나 모달의 바깥화면을 클릭해보세요.")
+    .then((value) => {
+      swal('반환된 값 : ${value}');
+    })
+  }
+
+  return (
+    <S.background className="App">
+      <S.btn onClick={style_alert}>클릭해보세요!</S.btn>
+      <S.btn onClick={alert_option}>클릭해보세요!</S.btn>
+      <S.btn onClick={alert_promise}>클릭해보세요!</S.btn>
+    </S.background>
+  );
+}
+
+export default App;
---
+++ react-library-test/src/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const background = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const btn = styled.button`
+    width: 10%;
+    height: 5%;
+    font-size: 20px;
+    margin: 15px;
+`
