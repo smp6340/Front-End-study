e6dca716c268e616c7840767a7ffcedc61f0085e
smp6340 <smp6340@naver.com>
2022-09-29 22:56:20 +0900

    ⚙️ : Next.js 개발 환경 재세팅

---
+++ next.js-study/components/index.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <header><h2>LOGO</h2></header>
+}
+
+export default Header;
---
+++ next.js-study/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Header/>
+    <div></div></>
+  <Footer/>
+)
+}
+
+export default Home
